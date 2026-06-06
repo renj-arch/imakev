b41c42f06cb49c2375a92f7e6b270cf244af88fe
renj-arch <renjith17740@gmail.com>
2026-06-06 09:52:25 +0530

    Switch to artistic sketch via pexels photos + opencv filter

--- sketch_test.py
+++ sketch_test.py
@@ -1,6 +1,6 @@
-import os, sys
-from src.sketch_engine import generate_sketch_scene
+import os
+from src.sketch_artist import draw_sketch_for_phrase
 prompt = os.environ.get('PROMPT', 'every night in my dreams')
-img = generate_sketch_scene(prompt, 720, 1280)
+img = draw_sketch_for_phrase(prompt, 720, 1280)
 img.save('hand_sketch_test.png')
 print(f'Saved: hand_sketch_test.png ({img.size[0]}x{img.size[1]})')
